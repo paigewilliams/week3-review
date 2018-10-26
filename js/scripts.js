31230ffc74c5c4151db45c4b4dcc985d6c7e49ad
Paige Williams <paw145@humboldt.edu>
2018-10-26 15:35:36 -0700

    Show output as unordered list

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,5 @@
 //---Business Logic---
 var numbersArray = [];
-var arrayBeep = []
 
 function createNumbers(inputNum) {
   var number = parseInt(inputNum);
@@ -9,43 +8,32 @@ function createNumbers(inputNum) {
     }
 
   for (var i = 0; i < numbersArray.length; i ++){
+
     if (numbersArray[i] === 0){
-      numbersArray.splice(numbersArray.indexOf(numbersArray[i]), 1, "beep")
+      numbersArray.splice(numbersArray.indexOf(numbersArray[i]), 1, "beep!")
     }
     if (numbersArray[i] === 1){
-      numbersArray.splice(numbersArray.indexOf(numbersArray[i]), 1, "boop")
+      numbersArray.splice(numbersArray.indexOf(numbersArray[i]), 1, "boop!")
     }
-    if (numbersArray[i] % 3==0){
+    if (numbersArray[i] % 3 == 0){
       numbersArray.splice(numbersArray.indexOf(numbersArray[i]), 1, "I'm sorry Dave, I'm afraid I can't do that.")
     }
-    if(numbersArray[i] >= 10 && numbersArray[i] %10==0){
-      numbersArray.splice(numbersArray.indexOf(numbersArray[i]), 1, "beep")
+    if(numbersArray[i] === 10 ){
+      numbersArray.splice(numbersArray.indexOf(numbersArray[i]), 1, "beep!")
     }
-    if(numbersArray[i] >= 11 && numbersArray[i] %11==0){
-      numbersArray.splice(numbersArray.indexOf(numbersArray[i]), 1, "boop")
+    if(numbersArray[i] >= 11 && numbersArray[i] %11 == 0){
+      numbersArray.splice(numbersArray.indexOf(numbersArray[i]), 1, "boop!")
     }
+    if(numbersArray[i] > 19 && numbersArray[i] %10 == 0){
+      numbersArray.splice(numbersArray.indexOf(numbersArray[i]), 1, "boop!")
+    }
+
+
   }
+
   return numbersArray;
 
-
-  // numbersArray.splice(numbersArray.indexOf(0), 1, "Beep!");
-  // numbersArray.splice(numbersArray.indexOf(1), 1, "Boop!");
-
-  // return number;
-
-  // if(numbersArray[i]=== "0"){
-  //   numbersArray.splice(numbersArray.indexOf(numbersArray[i]), 1, "Beep")
-  //     arrayBeep.push();
-  //     debugger
-
-
-
-
-
 }
-
-
-
 
 //---User Interface Logic---
 $(document).ready(function(){
@@ -55,8 +43,10 @@ $(document).ready(function(){
     var result = createNumbers(inputNum);
     console.log(result);
     // console.log(numbersArray);
-
-    $("#output").append('<li>' + numbersArray + '</li>');
+    result.forEach(function(num){
+    $("#outputList").append('<li>' + num + '</li>');
+    })
+    // $("#outputList").text(numbersArray);
     // console.log(inputNum);
 
     $(".output").show();
