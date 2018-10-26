d56a8ad620dd79214b807a888ccbec1645509607
Paige Williams <paw145@humboldt.edu>
2018-10-26 14:22:30 -0700

    Add conditonal to find number in array and replace with word.

--- js/scripts.js
+++ js/scripts.js
@@ -2,16 +2,33 @@
 var numbersArray = [];
 var arrayBeep = []
 
-function createNumber(inputNum) {
+function createNumbers(inputNum) {
   var number = parseInt(inputNum);
   for(var i= 0; i<= number; i++){
     numbersArray.push(i);
+    console.log(numbersArray);
+    }
+  // debugger
+  for (var i = 0; i < numbersArray.length; i ++){
+    if (numbersArray[i] === 0){
+      numbersArray.splice(numbersArray.indexOf(numbersArray[i]), 1, "beep")
+    }
+    // debugger
+    if (numbersArray[i] === 1){
+      numbersArray.splice(numbersArray.indexOf(numbersArray[i]), 1, "boop")
+    }
+
+    
+
+    // debugger
+    // return numbersArray.join(", ");
 
   }
 
-  numbersArray.splice(numbersArray.indexOf(0), 1, "Beep!");
-  numbersArray.splice(numbersArray.indexOf(1), 1, "Boop!");
   return numbersArray;
+
+  // numbersArray.splice(numbersArray.indexOf(0), 1, "Beep!");
+  // numbersArray.splice(numbersArray.indexOf(1), 1, "Boop!");
 
   // return number;
 
@@ -34,11 +51,11 @@ $(document).ready(function(){
   $("form#numberEnter").submit(function(event){
     event.preventDefault();
     var inputNum = ($("input#number").val());
-    var result = createNumber(inputNum);
+    var result = createNumbers(inputNum);
     console.log(result);
     // console.log(numbersArray);
 
-    $(".outputNumbers").html(numbersArray);
+    $(".outputNumbers").text(numbersArray);
     // console.log(inputNum);
 
     $(".output").show();
