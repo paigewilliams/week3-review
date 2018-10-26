091d4d40b5b83b8bddbf9055b946db02387c5719
Paige Williams <paw145@humboldt.edu>
2018-10-26 10:45:42 -0700

    Add function to create array of numbers, not working

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,47 @@
 //---Business Logic---
 var numbersArray = [];
 
+function countToNumber(number){
+  createArray();
+  
+  for (var i = 0; createArray.length; i ++){
+    if (createArray[i] > createArray){
+      numbersArray.push(createArray[i]);
+    }
+  }
+  return numbersArray;
+  console.log(numbersArray)
+}
+
   var createArray = function(inputNum) {
-    var numberToArray = inputNum.split("");
-    // console.log(numberToArray);
+    var numberToArray = parseInt(inputNum);
     return numberToArray;
-    // debugger;
+    debugger
   }
 
 
+  // function replace0(arrayofNumbers){
+  //   var array = createArray(number)
+  //   for (var = 0; i < arrayWit)
+  // }
 
 
 
+
+
+
+
+
+  // var arrayToIndvNum = function(numbertoArray){
+  //   createArray.forEach(function(numbertoArray){
+  //     parseInt();
+  //     debugger
+  //   })
+  //   return arrayToIndvNum;
+  //   console.log(arrayToIndvNum);
+  //
+  //   createArray();
+  // }
 
 
 
@@ -19,15 +49,12 @@ var numbersArray = [];
 $(document).ready(function(){
   $("form#numberEnter").submit(function(event){
     event.preventDefault();
-    var inputNum = $("input#number").val();
-
-
-
-    $(".outputNumbers").text(inputNum);
-    console.log(inputNum);
-
-    var result = createArray(inputNum);
+    var inputNum = ($("input#number").val());
+    var result = countToNumber(inputNum);
     console.log(result);
+
+    $(".outputNumbers").html(result);
+    // console.log(inputNum);
 
     $(".output").show();
 
