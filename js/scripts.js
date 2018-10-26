e191bd8a3ddb8a5bdb0d66817e17140f6cc43074
Paige Williams <paw145@humboldt.edu>
2018-10-26 14:44:39 -0700

    Add condtional to replace numbers greater than ten that contain a 1 or 0 with phrase.

--- js/scripts.js
+++ js/scripts.js
@@ -6,27 +6,27 @@ function createNumbers(inputNum) {
   var number = parseInt(inputNum);
   for(var i= 0; i<= number; i++){
     numbersArray.push(i);
-    // console.log(numbersArray);
     }
-  // debugger
+
   for (var i = 0; i < numbersArray.length; i ++){
-    if ((numbersArray[i] === 0 ) || (numbersArray[i] === 10 )) {
+    if (numbersArray[i] === 0){
       numbersArray.splice(numbersArray.indexOf(numbersArray[i]), 1, "beep")
     }
-    // debugger
     if (numbersArray[i] === 1){
       numbersArray.splice(numbersArray.indexOf(numbersArray[i]), 1, "boop")
     }
     if (numbersArray[i] % 3==0){
       numbersArray.splice(numbersArray.indexOf(numbersArray[i]), 1, "I'm sorry Dave, I'm afraid I can't do that.")
     }
-
-    // debugger
-    // return numbersArray.join(", ");
-
+    if(numbersArray[i] >= 10 && numbersArray[i] %10==0){
+      numbersArray.splice(numbersArray.indexOf(numbersArray[i]), 1, "beep")
+    }
+    if(numbersArray[i] >= 11 && numbersArray[i] %11==0){
+      numbersArray.splice(numbersArray.indexOf(numbersArray[i]), 1, "boop")
+    }
   }
-
   return numbersArray;
+
 
   // numbersArray.splice(numbersArray.indexOf(0), 1, "Beep!");
   // numbersArray.splice(numbersArray.indexOf(1), 1, "Boop!");
