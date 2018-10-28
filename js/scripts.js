b554e4336a03c1d94411bb4dd851519477d368a3
Paige Williams <paw145@humboldt.edu>
2018-10-28 11:11:59 -0700

    Start version 3 using includes method.

--- js/scripts.js
+++ js/scripts.js
@@ -1,52 +1,95 @@
 //---Business Logic---
+// var numbersArray = [];
+//
+// function createNumbers(inputNum) {
+//   var number = parseInt(inputNum);
+//   for(var i= 0; i<= number; i++){
+//     numbersArray.push(i);
+//     }
+//
+//   for (var i = 0; i < numbersArray.length; i ++){
+//
+//     if (numbersArray[i] === 0){
+//       numbersArray.splice(numbersArray.indexOf(numbersArray[i]), 1, "beep!")
+//     }
+//     if (numbersArray[i] === 1){
+//       numbersArray.splice(numbersArray.indexOf(numbersArray[i]), 1, "boop!")
+//     }
+//     if (numbersArray[i] % 3 == 0){
+//       numbersArray.splice(numbersArray.indexOf(numbersArray[i]), 1, "I'm sorry Dave, I'm afraid I can't do that.")
+//     }
+//     if(numbersArray[i] === 10 ){
+//       numbersArray.splice(numbersArray.indexOf(numbersArray[i]), 1, "beep!")
+//     }
+//     if(numbersArray[i] >= 11 && numbersArray[i] %11 == 0){
+//       numbersArray.splice(numbersArray.indexOf(numbersArray[i]), 1, "boop!")
+//     }
+//     if(numbersArray[i] > 19 && numbersArray[i] %10 == 0){
+//       numbersArray.splice(numbersArray.indexOf(numbersArray[i]), 1, "boop!")
+//     }
+//
+//
+//   }
+//
+//   return numbersArray;
+//
+// }
+//
+
+
+
+//---Version 2 Business Logic
+var zero = 0;
+var one = 1;
 var numbersArray = [];
-
 function createNumbers(inputNum) {
-  var number = parseInt(inputNum);
-  for(var i= 0; i<= number; i++){
+
+  for(var i= 0; i<= inputNum; i++){
     numbersArray.push(i);
-    }
-
-  for (var i = 0; i < numbersArray.length; i ++){
-
-    if (numbersArray[i] === 0){
-      numbersArray.splice(numbersArray.indexOf(numbersArray[i]), 1, "beep!")
-    }
-    if (numbersArray[i] === 1){
-      numbersArray.splice(numbersArray.indexOf(numbersArray[i]), 1, "boop!")
-    }
-    if (numbersArray[i] % 3 == 0){
-      numbersArray.splice(numbersArray.indexOf(numbersArray[i]), 1, "I'm sorry Dave, I'm afraid I can't do that.")
-    }
-    if(numbersArray[i] === 10 ){
-      numbersArray.splice(numbersArray.indexOf(numbersArray[i]), 1, "beep!")
-    }
-    if(numbersArray[i] >= 11 && numbersArray[i] %11 == 0){
-      numbersArray.splice(numbersArray.indexOf(numbersArray[i]), 1, "boop!")
-    }
-    if(numbersArray[i] > 19 && numbersArray[i] %10 == 0){
-      numbersArray.splice(numbersArray.indexOf(numbersArray[i]), 1, "boop!")
-    }
-
-
+    console.log(numbersArray);
   }
+   // var numbersToString = numbersArray.join('');
+   // console.log(numbersToString);
+    for (var i = 0; i <= numbersArray.length; i ++){
+      // debugger
+      if (numbersArray.includes(zero)){
+        numbersArray.splice(numbersArray.indexOf(numbersArray[i]), 1, "Beep!")
+         // debugger
+      }
+       else if (numbersArray.includes(one)){
+         numbersArray.splice(numbersArray.indexOf(numbersArray[i]), 1, "Boop!")
+       }
+       else if (numbersArray[i] % 3 == 0){
+         numbersArray.splice(numbersArray.indexOf(numbersArray[i]), 1, "I'm sorry Dave, I'm afraid I can't do that.")
+       }
+    }
+    return numbersArray;
+   }
 
-  return numbersArray;
 
-}
+
+    // console.log(numbersArray.includes(digit));
+
+
+    // console.log(numbersArray);
+
+
+
+
 
 //---User Interface Logic---
 $(document).ready(function(){
   $("form#numberEnter").submit(function(event){
     event.preventDefault();
-    var inputNum = ($("input#number").val());
+    var inputNum = parseInt($("input#number").val());
+
     var result = createNumbers(inputNum);
-    console.log(result);
+    // console.log(typeof inputNum);
     // console.log(numbersArray);
     result.forEach(function(num){
     $("#outputList").append('<li>' + num + '</li>');
     })
-    // $("#outputList").text(numbersArray);
+    $("#outputList").text(numbersArray);
     // console.log(inputNum);
 
     $(".output").show();
